8d8175e66957dd2e53a7d918655bee8120af5d67
naderibehnam717 <naderibehnam717@gmail.com>
2026-09-15 21:50:29 +0330

    اضافه کردن فایل

--- files.js
+++ files.js
@@ -64,5 +64,27 @@ const allFiles = [
         downloadUrl: "",
         downloadName: "جزوه-سیگنال-و-سیستم.pdf",
         color: "purple"
-    }
+    },
+    {
+        title: "جزوه مبانی کامپیوتر و برنامه‌نویسی",
+        desc: "جزوه کامل مبانی کامپیوتر و برنامه‌نویسی C++ — دانشگاه صنعتی بیرجند",
+        category: "کامپیوتر",
+        type: "جزوه",
+        level: "دانشگاهی",
+        viewUrl: "https://drive.google.com/file/d/15h2ylcOqKHGuvNsT1Yfdz0RezD21--jW/view",
+        downloadUrl: "https://drive.google.com/uc?export=download&id=15h2ylcOqKHGuvNsT1Yfdz0RezD21--jW",
+        downloadName: "جزوه-مبانی-کامپیوتر.pdf",
+        color: "blue"
+    },
+    {
+        title: "کتاب اندیشه اسلامی ۱",
+        desc: "کتاب اندیشه اسلامی ۱ — آیت‌الله جعفر سبحانی و دکتر محمد محمدی",
+        category: "معارف",
+        type: "کتاب",
+        level: "دانشگاهی",
+        viewUrl: "https://drive.google.com/file/d/10TbXwsw7XGSbEHpTrzkAb9VZ1NRPWZnh/view",
+        downloadUrl: "https://drive.google.com/uc?export=download&id=10TbXwsw7XGSbEHpTrzkAb9VZ1NRPWZnh",
+        downloadName: "کتاب-اندیشه-اسلامی-1.pdf",
+        color: "green"
+    },
 ];
