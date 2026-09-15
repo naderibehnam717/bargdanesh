d8ff859a8ab15939407e5d11c956d25cbec5243d
naderibehnam717 <naderibehnam717@gmail.com>
2026-09-15 18:09:06 +0330

    اولین آپلود

---
+++ files.js
@@ -0,0 +1,68 @@
+const allFiles = [
+    {
+        title: "جزوه فیزیک پایه",
+        desc: "جزوه کامل فیزیک ۱ و ۲",
+        category: "فیزیک",
+        type: "جزوه",
+        level: "دانشگاهی",
+        viewUrl: "https://drive.google.com/file/d/1XKAoerWekowO56UXQ_0iw3WwX3FN0Q60/view",
+        downloadUrl: "https://drive.google.com/uc?export=download&id=1XKAoerWekowO56UXQ_0iw3WwX3FN0Q60",
+        downloadName: "جزوه-فیزیک-پایه.pdf",
+        color: "purple"
+    },
+    {
+        title: "جزوه مبانی کامپیوتر",
+        desc: "جزوه کامل مبانی کامپیوتر و C++",
+        category: "کامپیوتر",
+        type: "جزوه",
+        level: "دانشگاهی",
+        viewUrl: "",
+        downloadUrl: "",
+        downloadName: "جزوه-مبانی-کامپیوتر.pdf",
+        color: "blue"
+    },
+    {
+        title: "کتاب اندیشه اسلامی ۱",
+        desc: "کتاب مرجع اندیشه اسلامی",
+        category: "معارف",
+        type: "کتاب",
+        level: "دانشگاهی",
+        viewUrl: "",
+        downloadUrl: "",
+        downloadName: "کتاب-اندیشه-اسلامی.pdf",
+        color: "green"
+    },
+    {
+        title: "کتاب آمار و مدیریت",
+        desc: "کتاب مرجع آمار، دکتر عادل آذر",
+        category: "آمار",
+        type: "کتاب",
+        level: "دانشگاهی",
+        viewUrl: "",
+        downloadUrl: "",
+        downloadName: "کتاب-آمار.pdf",
+        color: "rose"
+    },
+    {
+        title: "نمونه سوال ریاضی پایه اول",
+        desc: "نمونه سوال ریاضی پایه اول دبستان",
+        category: "ریاضی",
+        type: "نمونه سوال",
+        level: "مدرسه ای",
+        viewUrl: "",
+        downloadUrl: "",
+        downloadName: "نمونه-سوال-ریاضی.pdf",
+        color: "blue"
+    },
+    {
+        title: "جزوه سیگنال و سیستم",
+        desc: "جزوه سیگنال و سیستم، مهندس جواد خشت‌زر",
+        category: "مهندسی",
+        type: "جزوه",
+        level: "دانشگاهی",
+        viewUrl: "",
+        downloadUrl: "",
+        downloadName: "جزوه-سیگنال-و-سیستم.pdf",
+        color: "purple"
+    }
+];
